fe8f1af41fed898ef68141815bd063ed40b3c97c
Mxwsic <danielapacible2@gmail.com>
2023-04-06 02:00:42 -0400

    finished code

--- script.js
+++ script.js
@@ -2,22 +2,44 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $("#time9").val(localStorage.getItem("9AM"));
+  $("#time10").val(localStorage.getItem("10AM"));
+  $("#time11").val(localStorage.getItem("11AM"));
+  $("#time12").val(localStorage.getItem("12PM"));
+  $("#time13").val(localStorage.getItem("1PM"));
+  $("#time14").val(localStorage.getItem("2PM"));
+  $("#time15").val(localStorage.getItem("3PM"));
+  $("#time16").val(localStorage.getItem("4PM"));
+  $("#time17").val(localStorage.getItem("5PM"));
+  var now = dayjs().format("dddd, MMMM D YYYY");
+  console.log(now);
+  $("#currentDay").text(now);
+
+
+  $(".saveBtn").on("click", function() {
+    var hour = $(this).siblings(".hour").text();
+    localStorage.setItem($(this).siblings(".hour").text(), $(this).siblings(".description").val());
+  });
+
+
+  
+function changeTime(){
+    let currentHour = dayjs().hour();
+    console.log(currentHour);
+    $(".time-block").each(function (){
+      let Time = parseInt($(this).attr("data-time"));
+      console.log(Time);
+      if (Time < currentHour){
+        $(this).addClass("past");
+      } else if (Time === currentHour){
+        $(this).addClass("present");
+        $(this).removeClass("past");
+      } else {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      }
+    })
+}
+changeTime();
 });
